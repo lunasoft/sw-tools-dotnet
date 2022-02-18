49b32473b165140a57ad5d9c98d543732096de3e
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-02-17 20:21:04 -0600

    Convertir response v2 a v4
    - Se agrega metodo para realizar conversion de response de timbrado v2 a v4.
    - Se agrega la clase de las respuestas.

---
+++ SW.Tools/Entities/StampResponse/StampResponse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SW.Tools.Entities.StampResponse
+{
+    [DataContract]
+    public class StampResponse
+    {
+        [DataMember]
+        public string status { get; set; }
+    }
+    public class StampResponseV2 : StampResponse
+    {
+        [DataMember]
+        public StampResponse_Data_v2 data { get; set; }
+    }
+    public class StampResponseV4 : StampResponse
+    {
+        [DataMember]
+        public StampResponse_Data_v4 data { get; set; }
+    }
+    public class StampResponse_Data_v2
+    {
+        [DataMember]
+        public string tfd { get; set; }
+        [DataMember]
+        public string cfdi { get; set; }
+    }
+    public class StampResponse_Data_v4
+    {
+        [DataMember]
+        public string cadenaOriginalSAT { get; set; }
+        [DataMember]
+        public string noCertificadoSAT { get; set; }
+        [DataMember]
+        public string noCertificadoCFDI { get; set; }
+        [DataMember]
+        public string uuid { get; set; }
+        [DataMember]
+        public string selloSAT { get; set; }
+        [DataMember]
+        public string selloCFDI { get; set; }
+        [DataMember]
+        public string fechaTimbrado { get; set; }
+        [DataMember]
+        public string qrCode { get; set; }
+        [DataMember]
+        public string cfdi { get; set; }
+    }
+}
---
+++ SW.Tools/Services/Tool/Tool.cs
@@ -0,0 +1,24 @@
+﻿using SW.Tools.Services.Tool;
+using System;
+
+namespace SW.Tool
+{
+    public static class Tool
+    {
+        public static string ConvertResponseToV4(string response)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(response))
+                {
+                    return "El response no es valido.";
+                }
+                return ToolService.ConvertResponse(response);
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Error en la conversion.", e);
+            }
+        }
+    }
+}
