afae09665bba9c1dfe07b80066e238d7af3330b0
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-04-05 20:24:09 -0500

    Creacion de Cancelacion classes

---
+++ SW.Tools/Entities/Cancelacion/Cancelacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Entities.Cancelacion
+{
+    public class Cancelacion
+    {
+        public Guid Folio { get; set; }
+        public CancelacionMotivo Motivo { get; set; }
+        public Guid? FolioSustitucion { get; set; }
+    }
+}
---
+++ SW.Tools/Entities/Cancelacion/CancelacionMotivo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Entities.Cancelacion
+{
+    /// <summary>
+    /// Motivo de cancelación.
+    /// </summary>
+    public enum CancelacionMotivo
+    {
+        /// <summary>
+        /// Comprobante emitido con errores con relación.
+        /// </summary>
+        Motivo01 = 01,
+        /// <summary>
+        /// Comprobante emitido con errores sin relación.
+        /// </summary>
+        Motivo02 = 02,
+        /// <summary>
+        /// No se llevó a cabo la operación.
+        /// </summary>
+        Motivo03 = 03,
+        /// <summary>
+        /// Operación nominativa relacionada en la factura global.
+        /// </summary>
+        Motivo04 = 04
+    }
+}
