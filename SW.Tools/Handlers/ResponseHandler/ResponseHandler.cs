68d7e8350a03f02523c644acb7672595c7bd85e0
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-07-29 00:30:51 -0500

    Add Response Handlers
    - Se agregan handlers de las respuesta que se devolverán.

---
+++ SW.Tools/Handlers/ResponseHandler/ResponseHandler.cs
@@ -0,0 +1,19 @@
+﻿using SW.Tools.Entities.Response;
+using SW.Tools.Helpers;
+using System;
+using System.Security.Cryptography;
+
+namespace SW.Tools.Handlers.ResponseHandler
+{
+    internal class ResponseHandler
+    {
+        internal static Response HandleException(Exception ex, string message)
+        {
+            return ResponseHelper.GetResponse(ex.Message, ResponseHelper.GetErrorDetail(ex) ?? message);
+        }
+        internal static Response HandleException(CryptographicException ex, string message)
+        {
+            return ResponseHelper.GetResponse(ex.Message, ResponseHelper.GetErrorDetail(ex) ?? message);
+        }
+    }
+}
