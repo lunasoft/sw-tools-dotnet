16d609ae1bdce9505f0054635e5396739671dd45
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-07-29 00:33:36 -0500

    Add certificates services
    - Se agregan dos metodos para el servicio de certificados, uno para verificar si el certificado es FIEL o CSD y otro para leer la información relevante de un certificado.

---
+++ SW.Tools/Services/Certificates/Certificates.cs
@@ -0,0 +1,32 @@
+﻿using SW.Tools.Entities.Response;
+using System;
+using System.Security.Cryptography;
+
+namespace SW.Tools.Services.Certificates
+{
+    public class Certificates : CertificatesService
+    {
+        /// <summary>
+        /// Servicio que verifica si un certificado es de tipo FIEL, CSD o ninguno de los dos.
+        /// </summary>
+        /// <param name="certificado">Certificado.</param>
+        /// <exception cref="CryptographicException"></exception>
+        /// <exception cref="Exception"></exception>
+        /// <returns>Un objeto <see cref="Response"/></returns>
+        public static Response VerificarTipoCertificado(byte[] certificado)
+        {
+            return VerifyCertificateType(certificado);
+        }
+        /// <summary>
+        /// Servicio que lee y obtiene la información de un certificado.
+        /// </summary>
+        /// <param name="certificado">Certificado.</param>
+        /// <exception cref="CryptographicException"></exception>
+        /// <exception cref="Exception"></exception>
+        /// <returns>Un objeto <see cref="CertificatesResponse"/></returns>
+        public static CertificatesResponse LeerCertificado(byte[] certificado)
+        {
+            return ReadCertificate(certificado);
+        }
+    }
+}
---
+++ SW.Tools/Services/Certificates/CertificatesService.cs
@@ -0,0 +1,90 @@
+﻿using SW.Tools.Entities.Response;
+using SW.Tools.Handlers.ResponseHandler;
+using SW.Tools.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Services.Certificates
+{
+    public class CertificatesService
+    {
+        internal static Response VerifyCertificateType(byte[] cer)
+        {
+            try
+            {
+                X509Certificate2 x509Certificate = new X509Certificate2(cer);
+                List<X509KeyUsageExtension> extension = x509Certificate.Extensions.OfType<X509KeyUsageExtension>().ToList();
+                Response response = new Response();
+                if (isFIEL(extension))
+                {
+                    response.message = "El certificado es FIEL";
+                }
+                else
+                {
+                    if (isCSD(extension))
+                    {
+                        response.message = "El certificado es CSD";
+                    }
+                    else
+                    {
+                        response.status = "error";
+                        response.message = "El certificado no es CSD ni FIEL";
+                    }
+                }
+                return response;
+            }
+            catch(CryptographicException e)
+            {
+                return ResponseHandler.HandleException(e, "El certificado no es válido o está corrupto");
+            }
+            catch (Exception e)
+            {
+                return ResponseHandler.HandleException(e, "Ocurrió un error inesperado");
+            }
+        }
+        internal static CertificatesResponse ReadCertificate(byte[] cer)
+        {
+            try
+            {
+                CertificatesResponse response = new CertificatesResponse();
+                X509Certificate2 x509Certificate = new X509Certificate2(cer);
+                response.data = new CertificatesResponseData()
+                {
+                    commonName = x509Certificate.GetNameInfo(X509NameType.SimpleName, false),
+                    certificateNumber = SignUtils.CertificateNumber(x509Certificate),
+                    validFrom = x509Certificate.NotBefore,
+                    validTo = x509Certificate.NotAfter,
+                    issuer = x509Certificate.GetNameInfo(X509NameType.SimpleName, true),
+                    serialNumber = x509Certificate.SerialNumber,
+                };
+                return response;
+            } 
+            catch(CryptographicException e)
+            {
+                return CertificatesResponseHandler.HandleException(e, "El certificado no es válido o está corrupto");
+            } 
+            catch(Exception e)
+            {
+                return CertificatesResponseHandler.HandleException(e, "Ocurrió un error inesperado");
+            }
+        }
+        #region Private
+        private static bool isCSD(List<X509KeyUsageExtension> extension)
+        {
+            X509KeyUsageFlags flag = X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.NonRepudiation;
+            return extension[0].KeyUsages.HasFlag(flag);
+        }
+
+        private static bool isFIEL(List<X509KeyUsageExtension> extension)
+        {
+            X509KeyUsageFlags flag = X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.NonRepudiation | X509KeyUsageFlags.KeyAgreement | X509KeyUsageFlags.DataEncipherment;
+            return extension[0].KeyUsages.HasFlag(flag);
+        }
+        #endregion
+    }
+}
