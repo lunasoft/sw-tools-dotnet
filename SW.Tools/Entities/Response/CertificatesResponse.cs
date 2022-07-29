24687da2d5b2e7ebcd94a371b98d5819b4fd0716
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-07-29 00:30:08 -0500

    Agregar Response and Certificates Response.
    - Se agregan classes para las respuestas que se devolverán.

---
+++ SW.Tools/Entities/Response/CertificatesResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Entities.Response
+{
+    public class CertificatesResponse : Response
+    {
+        public CertificatesResponseData data { get; set; }
+    }
+    public class CertificatesResponseData
+    {
+        public string commonName { get; set; }
+        public string certificateNumber { get; set; }
+        public DateTime validFrom { get; set; }
+        public DateTime validTo { get; set; }
+        public string issuer { get; set; }
+        public string serialNumber { get; set; }
+    }
+}
---
+++ SW.Tools/Entities/Response/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Entities.Response
+{
+    public class Response
+    {
+        public string status { get; set; } = "success";
+        public string message { get; set; }
+        public string messageDetail { get; set; }
+    }
+}
