f712ac1f92c20e5fe8ef05c66b3f5a56bbe570f9
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-07-29 00:31:33 -0500

    Add Response Helpers.

---
+++ SW.Tools/Helpers/ResponseHelper.cs
@@ -0,0 +1,39 @@
+﻿using SW.Tools.Entities.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Helpers
+{
+    internal class ResponseHelper
+    {
+        internal static Response GetResponse(string message, string messageDetail)
+        {
+            return new Response()
+            {
+                status = "error",
+                message = message,
+                messageDetail = messageDetail
+            };
+        }
+        internal static CertificatesResponse GetCertificatesResponse(string message, string messageDetail)
+        {
+            return new CertificatesResponse()
+            {
+                status = "error",
+                message = message,
+                messageDetail = messageDetail
+            };
+        }
+        internal static string GetErrorDetail(Exception ex)
+        {
+            if (ex.InnerException != null)
+                return ex.InnerException.Message;
+            else
+                return null;
+        }
+    }
+}
