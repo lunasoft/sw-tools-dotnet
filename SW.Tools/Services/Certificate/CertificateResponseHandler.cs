16d1d97a66e86857ef63f09b589250bc84df4b3f
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-08-02 21:34:32 -0500

    Add Certificate response handler.

---
+++ SW.Tools/Services/Certificate/CertificateResponseHandler.cs
@@ -0,0 +1,34 @@
+﻿using SW.Tools.Entities.Response;
+using SW.Tools.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Handlers.CertificateResponseHandler
+{
+    internal class InfoCertificateResponseHandler : ResponseHandler<InfoCertificateResponse>
+    {
+        internal static InfoCertificateResponse HandleException(Exception e)
+        {
+            return GetResponse(e);
+        }
+        internal static InfoCertificateResponse HandleException(CryptographicException e)
+        {
+            return GetResponse(e);
+        }
+    }
+    internal class VerifyCertificateResponseHandler : ResponseHandler<VerifyCertificateResponse>
+    {
+        internal static VerifyCertificateResponse HandleException(Exception e)
+        {
+            return GetResponse(e);
+        }
+        internal static VerifyCertificateResponse HandleException(CryptographicException e)
+        {
+            return GetResponse(e);
+        }
+    }
+}
