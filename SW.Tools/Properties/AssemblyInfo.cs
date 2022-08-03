cdfda1a6cbcbfacc230dccfa467ad7061ecec946
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-08-02 21:35:11 -0500

    Update version.

--- SW.Tools/Properties/AssemblyInfo.cs
+++ SW.Tools/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.5.1")]
-[assembly: AssemblyFileVersion("1.0.5.1")]
+[assembly: AssemblyVersion("1.0.5.2")]
+[assembly: AssemblyFileVersion("1.0.5.2")]
