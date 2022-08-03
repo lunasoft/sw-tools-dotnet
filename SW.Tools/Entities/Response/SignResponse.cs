fb64b2109a8dc2987a2a227a0b3a8978d6bf85bd
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-08-03 12:41:05 -0500

    Add Sign Response class.

---
+++ SW.Tools/Entities/Response/SignResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Entities.Response
+{
+    public class SignResponse : Response
+    {
+        public SignDataResponse data { get; set; }
+    }
+    public partial class SignDataResponse
+    {
+        public string xml { get; set; }
+    }
+}
