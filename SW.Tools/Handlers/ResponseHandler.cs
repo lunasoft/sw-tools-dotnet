a0e3cb60a8c20f88565ddfb5b369c71eb109fabf
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-08-02 21:33:27 -0500

    Add Response handler.

---
+++ SW.Tools/Handlers/ResponseHandler.cs
@@ -0,0 +1,41 @@
+﻿using SW.Tools.Entities.Response;
+using SW.Tools.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Tools.Handlers
+{
+    internal class ResponseHandler<T> where T : Response, new ()
+    {
+        internal static T GetResponse(Exception e)
+        {
+            return TryGetResponse(e);
+        }
+        internal static T GetResponse(CryptographicException e)
+        {
+            return TryGetResponse(e);
+        }
+        private static T TryGetResponse(Exception e)
+        {
+            return new T()
+            {
+                status = "error",
+                message = e.Message,
+                messageDetail = ResponseHelper<Exception>.GetErrorDetail(e)
+            };
+        }
+        private static T TryGetResponse(CryptographicException e)
+        {
+            return new T()
+            {
+                status = "error",
+                message = e.Message,
+                messageDetail = ResponseHelper<CryptographicException>.GetErrorDetail(e)
+            };
+        }
+    }
+}
