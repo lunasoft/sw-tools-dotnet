9b7ac96430e668fad5017ed1537646419ad2abb3
Martin Flores <martin.flores@sw.com.mx>
2022-11-14 17:38:01 -0600

    hotfix cfdi40

--- SW.Tools/Properties/AssemblyInfo.cs
+++ SW.Tools/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("1.0.7.5")]
-[assembly: AssemblyFileVersion("1.0.7.5")]
+[assembly: AssemblyVersion("1.0.8.3")]
+[assembly: AssemblyFileVersion("1.0.8.3")]
